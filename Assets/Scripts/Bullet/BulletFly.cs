397306508c89bf8611e3d6f8f7b566db6bb6a0fa
Santino Casolati <63636985+santinocasolati@users.noreply.github.com>
2023-09-02 05:32:15 -0300

    Bullet Fly Time

--- Assets/Scripts/Bullet/BulletFly.cs
+++ Assets/Scripts/Bullet/BulletFly.cs
@@ -7,14 +7,29 @@ public class BulletFly : MonoBehaviour
     private Vector3 moveDirection;
     public float speed = 10f;
 
+    private float currentTime;
+    public float flyTime;
+
     public void SetDirection(Vector3 direction)
     {
         moveDirection = direction;
         moveDirection.z = transform.position.z;
     }
 
-    void Update()
+    private void HandleTimer()
+    {
+        currentTime += Time.deltaTime;
+
+        if (currentTime >= flyTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
     {
         transform.Translate(moveDirection.normalized * speed * Time.deltaTime);
+
+        HandleTimer();
     }
 }
